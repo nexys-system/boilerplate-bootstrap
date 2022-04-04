14728663d7c4f0d1c506313dd40dcf0928c7faab
Johan <johan@macair2020>
2022-04-04 14:31:53 +0200

    add basic bootstrap components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -15,4 +15,36 @@ export const Icon = ({ name }: { name: string }) => (
   <i className={`fa fa-${name}`}></i>
 );
 
+type Color =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "danger"
+  | "warning"
+  | "info"
+  | "light"
+  | "dark";
+
+interface NotificationProps {
+  children: JSX.Element | string;
+  color: Color;
+}
+
+// https://getbootstrap.com/docs/5.1/components/alerts/
+export const Alert = ({ children, color = "primary" }: NotificationProps) => (
+  <div className={`alert alert-${color}`} role="alert">
+    {children}
+  </div>
+);
+
+// https://getbootstrap.com/docs/5.1/components/badge/
+export const Badge = ({ children, color = "primary" }: NotificationProps) => (
+  <span className={`badge bg-${color}`}>{children}</span>
+);
+
+// https://getbootstrap.com/docs/5.1/components/badge/#pill-badges
+export const Pill = ({ children, color = "primary" }: NotificationProps) => (
+  <span className={`badge rounded-pill bg-${color}`}>{children}</span>
+);
+
 export const LoadDataAsync = LoadComponentAsyncGeneric(() => <Spinner />);
